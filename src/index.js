1af8b2f8202c06adbaeb8d81aa778b1767ba2d95
Onur Yıldırım <onur@cutepilot.com>
2017-09-30 05:26:49 +0300

    fix import typo

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import Notation from './core/Notation';
+import Notation from './core/notation';
 // export default Notation;
 // http://stackoverflow.com/a/33683495/112731
 module.exports = Notation;
