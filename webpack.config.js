ed7db558d78ecf73affd303ca40e2ada84d0f5c9
Onur Yıldırım <onur@cutepilot.com>
2017-09-30 04:53:59 +0300

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,108 +1,75 @@
-var webpack = require('webpack');
+const path = require('path');
+const webpack = require('webpack');
 
-// Webpack configuration
-// http://webpack.github.io/docs/configuration.html
-module.exports = function (params) {
-    var main = {
+const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+const plugins = [];
+
+const libraryName = 'Notation';
+const libraryFileName = 'notation.js';
+const libraryMinFileName = 'notation.min.js';
+
+let outputFile = libraryFileName;
+
+module.exports = env => {
+
+    if (env.NODE_ENV === 'production') {
+        // https://github.com/webpack-contrib/uglifyjs-webpack-plugin
+        plugins.push(new UglifyJsPlugin({
+            test: /\.js$/,
+            sourceMap: true,
+            uglifyOptions: {
+                ie8: false,
+                ecma: 5,
+                output: {
+                    comments: false,
+                    beautify: false
+                },
+                compress: true,
+                warnings: true
+            }
+        }));
+        outputFile = libraryMinFileName;
+    }
+
+    return {
         cache: false,
-        entry: params.entryPath,
-        // http://webpack.github.io/docs/configuration.html#devtool
+        entry: path.resolve(__dirname, 'src', 'index.js'),
         devtool: 'source-map',
         target: 'web',
         output: {
-            path: params.outPath,
-            filename: params.libFileName + '.js',
-            library: params.libName,
+            path: path.resolve(__dirname, 'lib'),
+            filename: outputFile,
+            library: libraryName,
             libraryTarget: 'umd',
             umdNamedDefine: true,
-            publicPath: params.publicPath
+            publicPath: 'lib/'
         },
         module: {
-            loaders: [
+            rules: [
                 {
-                    test: /(\.jsx|\.js)$/,
+                    test: /(\.jsx?)$/,
                     loader: 'babel-loader',
                     query: {
                         presets: ['es2015']
-                        // , plugins: [
-                        //     ['babel-plugin-transform-builtin-extend', {
-                        //         globals: ['Error', 'Array']
-                        //     }]
-                        // ]
                     },
                     exclude: /(node_modules|bower_components)/
+                },
+                {
+                    test: /\.html?$/,
+                    loader: 'html-loader'
                 }
-                // , {
-                //     test: /\.html?$/,
-                //     loader: 'html-loader'
-                // }
             ]
         },
         resolve: {
-            root: params.srcPath,
-            extensions: ['', '.js']
+            modules: [path.resolve(__dirname, 'src')],
+            extensions: ['.js']
         },
-        // Whether to show progress. Defaults to `true`.
-        progress: false,
         // Configure the console output.
         stats: {
             colors: true,
             modules: false,
             reasons: true
         },
-        // Whether to report error to grunt if webpack find errors. Use
-        // this if webpack errors are tolerable and grunt should
-        // continue.
-        failOnError: true,
-        // Use webpacks watcher. Requires the grunt process
-        // to be kept alive.
-        watch: false,
-        // Whether to finish the grunt task. Use this in combination
-        // with the watch option.
-        keepalive: false,
-        // Whether to embed the webpack-dev-server runtime into the
-        // bundle. Defaults to `false`.
-        inline: false
-    };
-
-    return {
-        main: main,
-        // examples:
-        // https://github.com/webpack/webpack-with-common-libs/blob/master/Gruntfile.js
-        // http://www.christianalfoni.com/articles/2015_04_19_The-ultimate-webpack-setup
-        server: {
-            options: {
-                webpack: main,
-                publicPath: '/' + params.publicPath
-                // , headers:{
-                //     'Access-Control-Allow-Origin': '*'
-                // }
-            },
-            start: {
-                keepAlive: true,
-                host: params.host,
-                port: params.port,
-                webpack: {
-                    entry: [
-                        // NOTE: Always the last one is exported.
-                        // source entry path
-                        params.entryPath
-                    ],
-                    devtool: 'eval',
-                    debug: true,
-                    watch: true,
-                    quite: false,
-                    noInfo: true,
-                    inline: true,
-                    historyApiFallback: true,
-                    plugins: [
-                        // Whether to add the HotModuleReplacementPlugin and
-                        // switch the server to hot mode. Use this in
-                        // combination with the inline option.
-                        new webpack.HotModuleReplacementPlugin()
-                    ]
-                }
-            }
-        }
+        plugins
     };
 };
