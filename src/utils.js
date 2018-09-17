125b924ded19630c3d65d2ac9a5cf1c716b7f61c
Onur Yıldırım <onur@cutepilot.com>
2018-09-17 16:13:02 +0300

    add/update utils

--- src/utils.js
+++ src/utils.js
@@ -1,14 +1,28 @@
 
-let toString = Object.prototype.toString;
+import NotationError from './core/notation.error';
+
+const reVAR = /^[a-z$_][a-z$_\d]*$/i;
+const reBRACKETS = /^\[(.*)\]$/;
+const reQUOTES = /^(?:'(.*)'|"(.*)"|`(.*)`)$/;
+const reESCAPE = /[.\\+*?[^\]$(){}=!<>|:-]/g;
+const oPROTO = Object.prototype;
 
 const utils = {
 
     isObject(o) {
-        return toString.call(o) === '[object Object]';
+        return oPROTO.toString.call(o) === '[object Object]';
     },
 
     isArray(o) {
-        return toString.call(o) === '[object Array]';
+        return oPROTO.toString.call(o) === '[object Array]';
+    },
+
+    isCollection(o) {
+        return utils.isObject(o) || utils.isArray(o);
+    },
+
+    isset(o) {
+        return o !== undefined && o !== null;
     },
 
     ensureArray(o) {
@@ -16,54 +30,110 @@ const utils = {
         return o === null || o === undefined ? [] : [o];
     },
 
-    hasOwn(o, prop) {
-        return o && typeof o.hasOwnProperty === 'function' && o.hasOwnProperty(prop);
+    hasOwn(collection, keyOrIndex) {
+        if (!collection) return false;
+        if (typeof keyOrIndex === 'string') {
+            return keyOrIndex && oPROTO.hasOwnProperty.call(collection, keyOrIndex);
+        }
+        if (utils.isArray(collection) && typeof keyOrIndex === 'number') {
+            return keyOrIndex >= 0 && keyOrIndex < collection.length;
+        }
+        return false;
     },
 
-    deepCopy(object) {
-        if (!utils.isObject(object)) return object;
-        let k, o,
-            copy = {};
-        for (k in object) {
-            if (utils.hasOwn(object, k)) {
-                o = object[k];
-                copy[k] = utils.isObject(o) ? utils.deepCopy(o) : o;
-            }
+    deepCopy(collection) {
+        if (utils.isObject(collection)) {
+            const copy = {};
+            Object.keys(collection).forEach(k => {
+                copy[k] = utils.deepCopy(collection[k]);
+            });
+            return copy;
         }
-        return copy;
+        if (utils.isArray(collection)) return collection.map(utils.deepCopy);
+        // not object or array
+        return collection;
     },
 
     // iterates over elements of an array, executing the callback for each
     // element.
     each(array, callback, thisArg) {
-        let length = array.length,
-            index = -1;
-        while (++index < length) {
-            if (callback.call(thisArg, array[index], index, array) === false) break;
+        const len = array.length;
+        let index = -1;
+        while (++index < len) {
+            if (callback.apply(thisArg, [array[index], index, array]) === false) break;
         }
     },
 
-    eachRight(array, callback) {
+    eachRight(array, callback, thisArg) {
         let index = array.length;
         while (index--) {
-            if (callback(array[index], index, array) === false) break;
+            if (callback.apply(thisArg, [array[index], index, array]) === false) break;
         }
     },
 
-    // Borrowed from http://phpjs.org/functions/preg_quote
-    pregQuote(str, delimiter) {
-        return String(str)
-            .replace(new RegExp('[.\\\\+*?\\[\\^\\]$(){}=!<>|:\\' + (delimiter || '') + '-]', 'g'), '\\$&');
+    eachProp(object, callback, thisArg) {
+        const keys = Object.keys(object);
+        let index = -1;
+        while (++index < keys.length) {
+            const key = keys[index];
+            if (callback.apply(thisArg, [object[key], key, object]) === false) break;
+        }
+    },
+
+    eachItem(collection, callback, thisArg) {
+        if (utils.isArray(collection)) {
+            return utils.each(collection, callback, thisArg);
+        }
+        return utils.eachProp(collection, callback, thisArg);
+    },
+
+    pregQuote(str) {
+        return String(str).replace(reESCAPE, '\\$&');
     },
 
     stringOrArrayOf(o, value) {
-        return (typeof o === 'string' && o === value)
-            || (utils.isArray(o) && o.length === 1 && o[0] === value);
+        return typeof value === 'string'
+            && (o === value
+                || (utils.isArray(o) && o.length === 1 && o[0] === value)
+            );
     },
 
     hasSingleItemOf(arr, itemValue) {
         return arr.length === 1
             && (arguments.length === 2 ? arr[0] === itemValue : true);
+    },
+
+    isArrayNote(note) {
+        return reBRACKETS.test(note);
+    },
+
+    // all validations might not be necessary here, since the full notation is
+    // always validated via `Notation.isValid()`.
+    normalizeNote(note) {
+        if (reVAR.test(note)) return note;
+        if (utils.isArrayNote(note)) {
+            // remove surrounding brackets
+            const n = note.replace(reBRACKETS, '$1');
+            // if integer only, this is an array index
+            if ((/^\d+$/).test(n)) return parseInt(n, 10);
+            // otherwise, it has to have surrounding quotes
+            if (reQUOTES.test(n)) return n.replace(reQUOTES, '$1$2$3');
+        }
+        throw new NotationError(`Invalid note: "${note}"`);
+    },
+
+    getNewNotation(newNotation, notation) {
+        const errMsg = `Invalid new notation: '${newNotation}'`;
+        // note validations (for newNotation and notation) are already made by
+        // other methods in the flow.
+        let newN;
+        if (typeof newNotation === 'string') {
+            newN = newNotation.trim();
+            if (!newN) throw new NotationError(errMsg);
+            return newN;
+        }
+        if (notation && !utils.isset(newNotation)) return notation;
+        throw new NotationError(errMsg);
     }
 
 };
