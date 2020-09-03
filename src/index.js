cdece132754d47249db5ee81e74a838f0b77619c
Onur Yıldırım <onur@cutepilot.com>
2020-09-03 19:20:42 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,2 @@
 /* istanbul ignore file */
-import Notation from './core/notation';
-// export default Notation;
-// http://stackoverflow.com/a/33683495/112731
-module.exports = Notation;
+export * from './core/notation';
